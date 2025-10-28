64432d1acc90d8649b81ade736a1ee2bad3825bc
Christian B. Knudsen <cbk@kb.dk>
2025-10-28 08:32:10 +0100

    dag 4 part 2 løst

--- aoc2022/day-04.R
+++ aoc2022/day-04.R
@@ -1,10 +1,11 @@
 # aoc 2022 day 4
+library(tidyverse)
 
 input <- readLines("aoc2022/data/day-04-input.txt")
 
-("2-4,6-8")
+# ("2-4,6-8")
 
-eval(parse(text = "2:4"))
+# eval(parse(text = "2:4"))
 
 
 example <- c(
@@ -27,6 +28,19 @@ overlap <- function(input){
 }
 
 sum(map_lgl(input, overlap))
-del 1 done
+# del 1 done
 
 
+# part 2 - any overlap
+
+overlap <- function(input){
+  input <- strsplit(base::gsub("-", ":", input), ",")
+  eval(parse(text = input[[1]][1]))
+  tmp <- list(eval(parse(text = input[[1]][1])), eval(parse(text = input[[1]][2])))
+  any(is.element(tmp[[1]], tmp[[2]]) ) | any(is.element(tmp[[2]], tmp[[1]]) )
+  
+}
+sum(map_lgl(example, overlap))
+sum(map_lgl(input, overlap))
+
+part 2 også løst
