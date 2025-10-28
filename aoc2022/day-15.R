97159c4a30e3ffca15f99ca6afd6cf9cdd6af2f6
Christian B. Knudsen <cbk@kb.dk>
2025-10-28 17:14:50 +0100

    noter

---
+++ aoc2022/day-15.R
@@ -0,0 +1,10 @@
+# aoc 2022 day 15
+
+Jeg skal konstruere en matrix
+der er negative tal i input. Så dem kan jeg nok med fordel 
+normere.
+
+Jeg skal placere en S og en B.
+
+Og jeg skal have en måde at markere alle celler med en manhattan afstand der er 
+mindre end manhattan afstanden mellem S og B
